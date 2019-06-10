616b2878e23b885d3a1498ea14531118e8fcaa91
Evoluxion9ja <evoluxion9ja@gmail.com>
2019-06-10 17:35:42 +0100

    Right aside registration layout done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-8 my-left-register">
                     <h1><strong>Register</strong></h1>
                     <div class="card">
                         <div class="card-header">
@@ -143,8 +143,87 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <h1>Already A Member?</h1>
+                <div class="col-md-4 my-right-register">
+                    <h1 class="invisible">Already A Member?</h1>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title"><strong>Have an Account?</strong></h5>
+                            <small><p>Sign in with an existing email</p></small>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <input id="email" type="email" placeholder="E-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <input id="password" type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn btn-outline-primary btn-block btn-sm">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 18rem; margin-top:7px;">
+                            <div class="card-body">
+                                <h5 class="card-title"><strong>Forgot Password?</strong></h5>
+                                <small><p>Enter your e-mail and click "send password". You will receive an e-mail containing a link to easily and securely reset your password.</p></small>
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+
+                                    <div class="form-group row">
+                                        <div class="col-md-12">
+                                            <input id="email" type="email" placeholder="E-mail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-12">
+                                            <button type="submit" class="btn btn-outline-primary btn-sm btn-block">
+                                                {{ __('Send Password Reset Link') }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
