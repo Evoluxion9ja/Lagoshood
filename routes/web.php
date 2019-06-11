3bf5e630d94931b4bcb9cc75bfcbde707427fde6
Evoluxion9ja <evoluxion9ja@gmail.com>
2019-06-11 10:05:05 +0100

    Route for category created

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Route For Pages
 Route::match(['GET', 'POST'], '/', [
     'uses' => 'PageController@index',
     'as' => '/'
+]);
+
+
+//Route For Category
+Route::match(['GET', 'POST'], '/categories', [
+    'uses' => 'CategoryController@index',
+    'as' => 'category.index'
+]);
+Route::match(['POST'], 'categories/store', [
+    'uses' => 'CategoryController@store',
+    'as' => 'category.store'
+]);
+Route::match(['GET', 'POST'], 'categories/{id}', [
+    'uses' => 'CategoryController@show',
+    'as' => 'category.show'
+]);
+Route::match(['PUT'], 'categories/{id}', [
+    'uses' => 'CategoryController@update',
+    'as' => 'category.update'
+]);
+Route::match(['DELETE'], 'categories/{id}', [
+    'uses' => 'CategoryController@destroy',
+    'as' => 'category.destroy'
 ]);
 
 Auth::routes();
